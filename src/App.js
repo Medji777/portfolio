bc3b0dfc6e36a80ba68c9c8140f8cbf8e8e50ebd
Medji <17dergachev@gmail.com>
2019-08-28 22:43:53 +0300

    Update App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HeaderContainer from "./components/Header/HeaderContainer";
+import MainSections from "./components/MainSections/MainSections";
+import './assets/fonts/fontsConfig';
+import AppContext from "./Context/Context";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <AppContext>
+            <HeaderContainer/>
+            <MainSections/>
+        </AppContext>
+    )
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React,{lazy,Suspense} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from "react-router-dom";
+import Loader from "./common/Loader/Loader";
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = lazy(() => import ("./App"));
+
+ReactDOM.render(<BrowserRouter>
+    <Suspense fallback={<Loader wrap={{minHeight: '100vh',background: '#404040'}} loader={{width: '15em'}}/>}>
+        <App />
+    </Suspense>
+</BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

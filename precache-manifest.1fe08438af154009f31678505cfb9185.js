1d09f19374d2078705a461c92922c604ac08e43f
Medji <17dergachev@gmail.com>
2019-08-28 23:13:44 +0300

    Updates

---
+++ precache-manifest.1fe08438af154009f31678505cfb9185.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd375b57b1a2db9c590db610079c1529",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8b8e900d079d878cb6a7",
+    "url": "/portfolio/static/css/4.ee94dc7a.chunk.css"
+  },
+  {
+    "revision": "d6c4aa812469a089fde8",
+    "url": "/portfolio/static/css/main.579f4f49.chunk.css"
+  },
+  {
+    "revision": "f91d7558ea11fd2968a1",
+    "url": "/portfolio/static/js/2.76071351.chunk.js"
+  },
+  {
+    "revision": "fe5cdc3750711cd7fbc4",
+    "url": "/portfolio/static/js/3.9371e5d7.chunk.js"
+  },
+  {
+    "revision": "8b8e900d079d878cb6a7",
+    "url": "/portfolio/static/js/4.94b973f9.chunk.js"
+  },
+  {
+    "revision": "d6c4aa812469a089fde8",
+    "url": "/portfolio/static/js/main.4b57d238.chunk.js"
+  },
+  {
+    "revision": "d2f1255c1858d7f95dd1",
+    "url": "/portfolio/static/js/runtime~main.3d160305.js"
+  },
+  {
+    "revision": "007808f3af8f970d1848ec8a0b5c6367",
+    "url": "/portfolio/static/media/prev_black.007808f3.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.ad9591c635c4ec5b679bc3c2cd57028b.js"
+  "/portfolio/precache-manifest.1fe08438af154009f31678505cfb9185.js"
 );
 
 self.addEventListener('message', (event) => {

1df0e43664c56583b727132bca9a3ead4732e6ee
Medji <17dergachev@gmail.com>
2019-08-28 19:19:35 +0300

    Create App Context

---
+++ src/Context/Context.js
@@ -0,0 +1,75 @@
+import React from 'react';
+
+const social = {
+    github: 'https://github.com/medji777',
+    vk: 'https://vk.com/medji777',
+    facebook: 'https://facebook.com/'
+};
+
+const projects = [
+    {
+        id: 1,
+        img: '#',
+        src: '#',
+        title: 'Social Network',
+        description: 'Lorem ipsum dolor sit amet, \n' +
+            'consectetur adipisicing elit. Aliquam aliquid aspernatur cupiditate distinctio, \n' +
+            'eius eos esse incidunt inventore nostrum possimus! Alias animi dolorem eaque \n' +
+            'earum error nemo sequi tenetur voluptatem!',
+        filter: 'SPA'
+    },
+    {
+        id: 2,
+        img: '#',
+        src: '#',
+        title: 'Todolist',
+        description: 'Lorem ipsum dolor sit amet, \n' +
+            'consectetur adipisicing elit. Aliquam aliquid aspernatur cupiditate distinctio, \n' +
+            'eius eos esse incidunt inventore nostrum possimus! Alias animi dolorem eaque \n' +
+            'earum error nemo sequi tenetur voluptatem!',
+        filter: 'SPA'
+    },
+    {
+        id: 3,
+        img: '#',
+        src: '#',
+        title: 'Html',
+        description: 'Lorem ipsum dolor sit amet, \n' +
+            'consectetur adipisicing elit. Aliquam aliquid aspernatur cupiditate distinctio, \n' +
+            'eius eos esse incidunt inventore nostrum possimus! Alias animi dolorem eaque \n' +
+            'earum error nemo sequi tenetur voluptatem!',
+        filter: 'JS'
+    }
+];
+
+const progress = [
+    {title: 'HTML | CSS3', progress: 85},
+    {title: 'JavaScript - ES5/6/7/8/9', progress: 96},
+    {title: 'React | Redux', progress: 88},
+    {title: 'TypeScript', progress: 86},
+    {title: 'Webpack | Gulp', progress: 78},
+    {title: 'NPM | GIT', progress: 89}
+];
+
+const menuLink = [
+    {id: 1, exact: true, path: '/', title: 'Main'},
+    {id: 2, exact: false, path: '/skills', title: 'Skills'},
+    {id: 3, exact: false, path: '/projects', title: 'Projects'},
+    {id: 4, exact: false, path: '/contacts', title: 'Contacts'},
+];
+
+const AppContext = ({children}) => {
+    return (
+        <Context.Provider value={{
+            menuLink,
+            social,
+            projects,
+            progress
+        }}>
+            {children}
+        </Context.Provider>
+    )
+};
+
+export const Context = React.createContext(null);
+export default AppContext;

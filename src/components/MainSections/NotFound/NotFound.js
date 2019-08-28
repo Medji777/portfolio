78a87dc9ee68ec3af6a7e0bd1669024f7dab532c
Medji <17dergachev@gmail.com>
2019-08-28 21:41:14 +0300

    Create Not Found page components

---
+++ src/components/MainSections/NotFound/NotFound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Mouse3DHoc from "../../Mouse3dHOC/Mouse3dHoc";
+import s from './NotFound.module.css';
+
+const NotFound = ({onStart3d, onStop3d}) => {
+    return (
+        <section className={`${s.notfound} ${s.main}`}>
+            <div className={s.container}>
+                <div className={s.wrapper} onMouseMove={onStart3d} onMouseLeave={onStop3d}>
+                    <h2>404 Not Found</h2>
+                    <span>404 Not Found</span>
+                </div>
+            </div>
+        </section>
+    )
+};
+
+export default Mouse3DHoc(NotFound);

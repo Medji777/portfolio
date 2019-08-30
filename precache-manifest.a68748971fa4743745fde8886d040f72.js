f682bc865d78f9c3eb9d5aa16dab23de9367f37d
Medji <17dergachev@gmail.com>
2019-08-30 18:24:23 +0300

    Updates

--- precache-manifest.a68748971fa4743745fde8886d040f72.js
+++ precache-manifest.a68748971fa4743745fde8886d040f72.js
@@ -1,14 +1,14 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8a894611fbb812a37759c5bf11f8fddc",
+    "revision": "1148e407c7d248e650d8a5336be2ac93",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "a59f6c14df89b862100e",
+    "revision": "d8ef2b3fa3e4f97c23c9",
     "url": "/portfolio/static/css/4.e031261a.chunk.css"
   },
   {
-    "revision": "a4fdf7bce85863628a7e",
+    "revision": "aa1ff3089c12da509058",
     "url": "/portfolio/static/css/main.579f4f49.chunk.css"
   },
   {
@@ -20,16 +20,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/3.9371e5d7.chunk.js"
   },
   {
-    "revision": "a59f6c14df89b862100e",
-    "url": "/portfolio/static/js/4.8b16c4eb.chunk.js"
+    "revision": "d8ef2b3fa3e4f97c23c9",
+    "url": "/portfolio/static/js/4.0f12ca57.chunk.js"
   },
   {
-    "revision": "a4fdf7bce85863628a7e",
-    "url": "/portfolio/static/js/main.38e0e478.chunk.js"
+    "revision": "aa1ff3089c12da509058",
+    "url": "/portfolio/static/js/main.e3618b43.chunk.js"
   },
   {
-    "revision": "91bfa9d42168ff244d12",
-    "url": "/portfolio/static/js/runtime~main.d7b526ae.js"
+    "revision": "a9668835cf40e464fc54",
+    "url": "/portfolio/static/js/runtime~main.27358bd7.js"
   },
   {
     "revision": "007808f3af8f970d1848ec8a0b5c6367",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.55463c8b9b5cada3ed5a049aeb419a80.js"
+  "/portfolio/precache-manifest.a68748971fa4743745fde8886d040f72.js"
 );
 
 self.addEventListener('message', (event) => {

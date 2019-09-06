a10506970ffca870dfe195bcc8093c8646894f6d
Medji <17dergachev@gmail.com>
2019-09-07 00:25:11 +0300

    Refactoring bug

--- src/components/MainSections/Skills/Progress/ProgressContainer.js
+++ src/components/MainSections/Skills/Progress/ProgressContainer.js
@@ -13,15 +13,19 @@ class ProgressContainer extends React.Component {
     componentDidUpdate(prevProps, prevState) {
         if(this.state.isLoad){
             this.setState({isLoad: false});
-            let interval = setInterval(()=>{
+            this.interval = setInterval(()=>{
                 this.setState({count: this.state.count + 1});
                 if(this.state.count <= this.props.progress){
                     this.fill.current && (this.fill.current.style.width = this.state.count + '%');
                 } else {
-                    clearInterval(interval);
+                    clearInterval(this.interval);
                 }
             },0);
         }
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
     }
 
     render(){

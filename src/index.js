24429e50c61c817acfcabf932b59b26aa28cf8b3
Medji <17dergachev@gmail.com>
2019-09-07 00:27:39 +0300

    Minor update file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React,{lazy,Suspense} from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter as Router} from "react-router-dom";
 import Loader from "./common/Loader/Loader";
 import './index.css';
 
 const App = lazy(() => import ("./App"));
 
-ReactDOM.render(<BrowserRouter>
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>
     <Suspense fallback={<Loader wrap={{minHeight: '100vh',background: '#404040'}} loader={{width: '15em'}}/>}>
         <App />
     </Suspense>
-</BrowserRouter>, document.getElementById('root'));
+</Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
